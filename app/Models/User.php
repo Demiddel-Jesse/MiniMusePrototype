831373f8cb1aa01c111cbf068a5e355fb4792bee
Demiddel-Jesse <jesse.demiddel19@gmail.com>
2024-08-19 15:19:13 +0200

    adjusted attributes of user

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,9 +18,10 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'username',
         'email',
         'password',
+        'profile_picture'
     ];
 
     /**
@@ -31,6 +32,7 @@ class User extends Authenticatable
     protected $hidden = [
         'password',
         'remember_token',
+        'admin',
     ];
 
     /**
