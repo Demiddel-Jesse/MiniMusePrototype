fd017ba567fc9933d504f71270873090fca2db4d
Demiddel-Jesse <jesse.demiddel19@gmail.com>
2024-08-19 15:46:30 +0200

    created factory and seeder for Image

---
+++ database/factories/ImageFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Post;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Image>
+ */
+class ImageFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $faker = \Faker\Factory::create();
+        $faker->addProvider(new \Smknstd\FakerPicsumImages\FakerPicsumImagesProvider($faker));
+
+        return [
+            'path' => $faker->imageUrl(width: 1600, height: 1600),
+            'post_id' => Post::all()->random()->id,
+        ];
+    }
+}
