d0c4d79208e69865fa7356f816eb7669679e0302
Demiddel-Jesse <jesse.demiddel19@gmail.com>
2024-08-20 08:20:06 +0200

    added basic relations between Image Post and User

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Model;
 
 class Image extends Model
@@ -18,4 +19,14 @@ class Image extends Model
         'path',
         'post_id'
     ];
+
+    /**
+     * Get the post that owns the Image
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function post(): BelongsTo
+    {
+        return $this->belongsTo(Post::class);
+    }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
@@ -21,4 +23,24 @@ class Post extends Model
         'private',
         'user_id',
     ];
+
+    /**
+     * Get all of the images for the Post
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function images(): HasMany
+    {
+        return $this->hasMany(Image::class);
+    }
+
+    /**
+     * Get the user that owns the Post
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
