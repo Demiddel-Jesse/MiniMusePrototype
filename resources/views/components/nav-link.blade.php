a5ecde0849e961497bb4d83e2d5e5d85f92d3520
Demiddel-Jesse <jesse.demiddel19@gmail.com>
2024-08-22 13:45:28 +0200

    fixed active element being wrong

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -2,8 +2,8 @@
 
 @php
 $classes = ($active ?? false)
-? 'c-nav__link'
-: 'c-nav__link c-nav__link--active';
+? 'c-nav__link c-nav__link--active'
+: 'c-nav__link ';
 @endphp
 
 <a {{ $attributes->merge(['class' => $classes]) }}>
