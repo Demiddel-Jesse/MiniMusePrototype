9dcf13932fedae3501fee0782818b062d22ddab3
Demiddel-Jesse <jesse.demiddel19@gmail.com>
2024-08-22 09:50:06 +0200

    added favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,31 +6,26 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
 
     <!-- Scripts -->
     @vite(['resources/scss/app.scss', 'resources/js/app.js'])
 </head>
-<body class="">
-    <div class="">
-        @include('layouts.navigation')
+<body>
+    @include('layouts.navigation')
 
-        <!-- Page Heading -->
-        @if (isset($header))
-        <header class="">
-            <div class="">
-                {{ $header }}
-            </div>
-        </header>
-        @endif
+    <!-- Page Heading -->
+    @if (isset($header))
+    <header class="">
+        <div class="">
+            {{ $header }}
+        </div>
+    </header>
+    @endif
 
-        <!-- Page Content -->
-        <main>
-            {{ $slot }}
-        </main>
-    </div>
+    <!-- Page Content -->
+    <main>
+        {{ $slot }}
+    </main>
 </body>
 </html>
