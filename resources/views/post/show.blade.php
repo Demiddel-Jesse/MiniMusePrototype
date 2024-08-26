8732177ef245eebc7cc6158798c79104c5c2b7a8
Demiddel-Jesse <jesse.demiddel19@gmail.com>
2024-08-26 15:45:12 +0200

    added tags and tagTypes to post

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -7,7 +7,27 @@
         </div>
         <div class="c-post__text">
             <h1>{{ $post->title }}</h1>
+            <div class="c-post__profile">
+                <img src="{{ $post->user->profile_picture }}" alt="{{ $post->user->username }} Profile Picture">
+                <p>{{ $post->user->username }}</p>
+            </div>
+            <div class="c-post__numbers"></div>
             <p>{{ $post->description }}</p>
+
+            @php
+            $tagsByType = $post->tags->groupBy('tag_type_id');
+            @endphp
+
+            @foreach($tagTypes as $tagType)
+            @if(isset($tagsByType[$tagType->id]))
+            <h3>{{ $tagType->name }}</h3>
+            @foreach($tagsByType[$tagType->id] as $tag)
+            <p>{{ $tag->name }}</p>
+            @endforeach
+            @endif
+            @endforeach
+
+
         </div>
     </section>
 </x-app-layout>
