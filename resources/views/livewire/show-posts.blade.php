bbc21867fc023a31b0804909c94972987f53201b
Demiddel-Jesse <jesse.demiddel19@gmail.com>
2024-08-27 15:34:27 +0200

    added button for loadmore

--- resources/views/livewire/show-posts.blade.php
+++ resources/views/livewire/show-posts.blade.php
@@ -7,6 +7,7 @@
     </section>
     @if($posts->hasMorePages())
     <span x-intersect="$wire.loadMore()"></span>
+    <button wire:click.prevent="loadMore" class="c-button c-button__secondary">Load more</button>
     @else
     <p class="c-gallery__noMore">Looks like there are no more posts...</p>
     @endif
